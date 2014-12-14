f33cbca45675fce8148f07ec076d08a378c7d924
flint-stone <sharonxu65@gmail.com>
2014-12-13 23:49:06 -0600

    new

--- src/jvm/storm/elasticity/ProcessingTopology.java
+++ src/jvm/storm/elasticity/ProcessingTopology.java
@@ -12,11 +12,11 @@ import backtype.storm.topology.TopologyBuilder;
 public class ProcessingTopology {
 	public static void main(String[] args) throws Exception {
 		//int numBolt = 3;
-		int paralellism = 2;
+		int paralellism = 4;
 
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("spout_head", new RandomLogSpout(), paralellism);
+		builder.setSpout("spout_head", new RandomLogSpout(), paralellism).setNumTasks(8);
 
 		/*for (int i = 0; i < numBolt; i++) {
 			if (i == 0) {
@@ -35,22 +35,22 @@ public class ProcessingTopology {
 			}
 		}*/
 		//builder.setBolt("bolt_transform", new TransformBolt(), paralellism).shuffleGrouping("spout_head");
-		builder.setBolt("bolt_transform", new TransformBolt(), paralellism+2).shuffleGrouping("spout_head");
-		builder.setBolt("bolt_filter", new FilterBolt(), paralellism+2).shuffleGrouping("bolt_transform");
-		builder.setBolt("bolt_join1", new TestBolt(), paralellism+2).shuffleGrouping("bolt_filter");
-		builder.setBolt("bolt_output_1", new TestBolt(),paralellism+2).shuffleGrouping("bolt_join1");
+		builder.setBolt("bolt_transform", new TransformBolt(), paralellism+2).setNumTasks(8).shuffleGrouping("spout_head");
+		builder.setBolt("bolt_filter", new FilterBolt(), paralellism+2).setNumTasks(8).shuffleGrouping("bolt_transform");
+		builder.setBolt("bolt_join1", new TestBolt(), paralellism+2).setNumTasks(8).shuffleGrouping("bolt_filter");
+		builder.setBolt("bolt_output_1", new TestBolt(),paralellism+2).setNumTasks(8).shuffleGrouping("bolt_join1");
 		//builder.setBolt("bolt_filter_2", new FilterBolt(), paralellism).shuffleGrouping("bolt_join");
 		//builder.setBolt("bolt_aggregate", new AggregationBolt(), paralellism).shuffleGrouping("bolt_filter_2");
-		builder.setBolt("bolt_join2", new TransformBolt(), paralellism+2).shuffleGrouping("bolt_transform");
-		builder.setBolt("bolt_normalize", new TestBolt(),paralellism+2).shuffleGrouping("bolt_join2");
-		builder.setBolt("bolt_output_2", new TestBolt(),paralellism+2).shuffleGrouping("bolt_normalize");
+		builder.setBolt("bolt_join2", new TransformBolt(), paralellism+2).setNumTasks(8).shuffleGrouping("bolt_transform");
+		builder.setBolt("bolt_normalize", new TestBolt(),paralellism+2).setNumTasks(8).shuffleGrouping("bolt_join2");
+		builder.setBolt("bolt_output_2", new TestBolt(),paralellism+2).setNumTasks(8).shuffleGrouping("bolt_normalize");
 
 		Config conf = new Config();
 		conf.setDebug(true);
 
 		conf.setNumAckers(0);
 
-		conf.setNumWorkers(12);
+		conf.setNumWorkers(32);
 
 		StormSubmitter.submitTopologyWithProgressBar(args[0], conf,
 				builder.createTopology());
